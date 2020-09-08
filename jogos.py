d0391f11cb13f8c85f1fe9534f1da0ad80ac9e51
busatoju <70058840+busatoju@users.noreply.github.com>
2020-09-08 16:43:22 -0300

    Incluída a chamada dos jogos

--- jogos.py
+++ jogos.py
@@ -15,10 +15,10 @@ while(jogo < 1 or jogo > 2):
 
 if(jogo == 1):
     print("Jogo escolhido: Adivinhação")
-    adivinhacao.adivinhacao.jogar()
+    adivinhacao.jogar()
 elif(jogo == 2):
     print("Jogo escolhido: Forca")
-    forca.forca.jogar()
+    forca.jogar()
 
 else:
     while(jogo < 1 or jogo > 2):
