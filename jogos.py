c9f2e63d5d273a1b05e625e48d65f7692184112d
busatoju <70058840+busatoju@users.noreply.github.com>
2020-09-08 18:14:33 -0300

    Menu inserido dentro de uma função

--- jogos.py
+++ jogos.py
@@ -1,28 +1,33 @@
 import forca
 import adivinhacao
 
-print("Escolha o seu jogo:")
-print("1 - Adivinhcação")
-print("2 - Forca")
 
-jogo = int(input("Qual você quer jogar?"))
-
-while(jogo < 1 or jogo > 2):
+def menu_jogos():
     print("Escolha o seu jogo:")
     print("1 - Adivinhcação")
     print("2 - Forca")
+
     jogo = int(input("Qual você quer jogar?"))
 
-if(jogo == 1):
-    print("Jogo escolhido: Adivinhação")
-    adivinhacao.jogar()
-elif(jogo == 2):
-    print("Jogo escolhido: Forca")
-    forca.jogar()
-
-else:
     while(jogo < 1 or jogo > 2):
         print("Escolha o seu jogo:")
         print("1 - Adivinhcação")
         print("2 - Forca")
         jogo = int(input("Qual você quer jogar?"))
+
+    if(jogo == 1):
+        print("Jogo escolhido: Adivinhação")
+        adivinhacao.jogar()
+    elif(jogo == 2):
+        print("Jogo escolhido: Forca")
+        forca.jogar()
+
+    else:
+        while(jogo < 1 or jogo > 2):
+            print("Escolha o seu jogo:")
+            print("1 - Adivinhcação")
+            print("2 - Forca")
+            jogo = int(input("Qual você quer jogar?"))
+
+
+menu_jogos()
